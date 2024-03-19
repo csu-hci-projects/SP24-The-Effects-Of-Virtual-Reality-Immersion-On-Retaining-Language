4ac30ca1b20e201a878f6b42868fa4a753728cb5
Jake <jakeparra98@yahoo.com>
2024-03-19 12:11:18 -0600

    Adds functionality to buttons

--- FlashCardProgram/FlashCardProgram/src/DrawButton/DrawButton.js
+++ FlashCardProgram/FlashCardProgram/src/DrawButton/DrawButton.js
@@ -6,16 +6,28 @@ class DrawButton extends Component{
         super(props);
 
         this.drawCard = this.drawCard.bind(this);
+        this.handleNextCard = this.handleNextCard.bind(this);
+        this.handlePrevCard = this.handlePrevCard.bind(this);
     }
 
     drawCard(){
         this.props.drawCard();
     }
     
+    handleNextCard(){
+        this.props.getNextCard();
+    }
+
+    handlePrevCard(){
+        this.props.getPrevCard();
+    }
+
     render(props){
         return(
             <div className= "buttonContainer">
+                <button className="button" onClick={this.handlePrevCard}>&#x2B05;</button>
                 <button className="button" onClick={this.drawCard}>Random Card</button>
+                <button className="button" onClick={this.handleNextCard}>&#x2B95;</button>
             </div>
         )
     }
